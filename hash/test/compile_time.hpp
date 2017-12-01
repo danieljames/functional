ef7315a08a050de1635ab94be0f123df48e069dd
Daniel James <daniel@calamity.org.uk>
2017-12-01 15:17:54 +0000

    Check that hash is nothrow move assignable etc.

--- hash/test/compile_time.hpp
+++ hash/test/compile_time.hpp
@@ -7,12 +7,26 @@
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_base_and_derived.hpp>
 
+#if !defined(BOOST_NO_CXX11_NOEXCEPT)
+#include <boost/type_traits/is_nothrow_move_assignable.hpp>
+#include <boost/type_traits/is_nothrow_move_constructible.hpp>
+// TODO: Uncomment when my merge request is accepted.
+//#include <boost/type_traits/is_nothrow_swappable.hpp>
+
+#endif
+
 template <class T>
 void compile_time_tests(T*)
 {
 #if !defined(_HAS_AUTO_PTR_ETC) || _HAS_AUTO_PTR_ETC
     BOOST_STATIC_ASSERT((boost::is_base_and_derived<
         std::unary_function<T, std::size_t>, BOOST_HASH_TEST_NAMESPACE::hash<T> >::value));
+#endif
+
+#if !defined(BOOST_NO_CXX11_NOEXCEPT)
+    BOOST_STATIC_ASSERT((boost::is_nothrow_move_assignable<boost::hash<T> >::value));
+    BOOST_STATIC_ASSERT((boost::is_nothrow_move_constructible<boost::hash<T> >::value));
+    //BOOST_STATIC_ASSERT((boost::is_nothrow_swappable<boost::hash<T> >::value));
 #endif
 }
 
